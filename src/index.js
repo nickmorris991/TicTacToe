a1b5cb2cb5c858fcb41121947bf2fe9e4a4b2c95
nickmorris991 <nickmorris25@outlook.com>
2020-01-14 14:28:41 -0500

    added time travel to this branch (finished) stil need to fix some bugs and styling though

--- src/index.js
+++ src/index.js
@@ -101,13 +101,14 @@ class Game extends React.Component {
       history: [{
         squares: Array(9).fill(null),
       }],
+      stepNumber: 0,
       xIsNext: true,
     };
   }
 
   handleClick(i) {
-    const history = this.state.history;
-    const current = history[history.length -1];
+    const history = this.state.history.slice(0, this.state.stepNumber + 1);
+    const current = history[this.state.stepNumber];
     const squares = current.squares.slice();
     
     if (calculateWinner(squares) || squares[i]) {
@@ -118,7 +119,15 @@ class Game extends React.Component {
       history: history.concat([{
         squares: squares,
       }]),
+      stepNumber: history.length,
       xIsNext: !this.state.xIsNext,
+    });
+  }
+
+  jumptTo(step) {
+    this.setState({
+      stepNumber: step,
+      xIsNext: (step % 2) === 0,
     });
   }
 
@@ -134,8 +143,8 @@ class Game extends React.Component {
         'Go to move #' + move :
         'go to game start';
       return (
-        <li>
-          <div className='button'><button onClick={() => this.jumpto(move)}>{desc}</button></div>
+        <li key={move}>
+          <div className='button'><button onClick={() => this.jumpTo(move)}>{desc}</button></div>
         </li>
       )
     });
